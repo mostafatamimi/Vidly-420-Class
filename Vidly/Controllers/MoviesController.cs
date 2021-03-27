aec7a2e3ce3502d5b9b4539fee1e0382e6a3ee1f
mosta <mosta@MSI>
2021-03-27 09:28:56 +0200

    First Push

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+        public ActionResult Index()
+        {
+
+            var movie = new Movie() { Name = "Titans!",id=123 };
+            ViewBag.TempName = "Hello World";
+
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home");
+
+            return View(movie);
+        }
+    }
+}
